8196e8cc2a16320e39ed3ed0c228207d2505b8a1
zak aden <faf>
2021-08-23 14:32:40 +0100

    data added to seperate file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { morse, alphabet } from "./englishData.js"
+
 const userEnglishInput = document.querySelector('.userEnglish')
 const englishButton = document.querySelector('.englishButton')
 const returnEngTranslation = document.querySelector('.returnEngTranslation')
@@ -6,126 +8,37 @@ const userMorseInput = document.querySelector('.userMorse')
 const morseButton = document.querySelector('.morseButton')
 const returnMorseTranslation = document.querySelector('.returnMorseTranslation')
 
-const morse = {
-	'-----': '0',
-	'.----': '1',
-	'..---': '2',
-	'...--': '3',
-	'....-': '4',
-	'.....': '5',
-	'-....': '6',
-	'--...': '7',
-	'---..': '8',
-	'----.': '9',
-	'.-': 'a',
-	'-...': 'b',
-	'-.-.': 'c',
-	'-..': 'd',
-	'.': 'e',
-	'..-.': 'f',
-	'--.': 'g',
-	'....': 'h',
-	'..': 'i',
-	'.---': 'j',
-	'-.-': 'k',
-	'.-..': 'l',
-	'--': 'm',
-	'-.': 'n',
-	'---': 'o',
-	'.--.': 'p',
-	'--.-': 'q',
-	'.-.': 'r',
-	'...': 's',
-	'-': 't',
-	'..-': 'u',
-	'...-': 'v',
-	'.--': 'w',
-	'-..-': 'x',
-	'-.--': 'y',
-	'--..': 'z',
-	'/': ' ',
-	'-·-·--': '!',
-	'·-·-·-': '.',
-	'--··--': ',',
-};
 
 
-const alphabet = {
-	0: '-----',
-	1: '.----',
-	2: '..---',
-	3: '...--',
-	4: '....-',
-	5: '.....',
-	6: '-....',
-	7: '--...',
-	8: '---..',
-	9: '----.',
-	'a': '.-',
-	'b': '-...',
-	'c': '-.-.',
-	'd': '-..',
-	'e': '.',
-	'f': '..-.',
-	'g': '--.',
-	'h': '....',
-	'i': '..',
-	'j': '.---',
-	'k': '-.-',
-	'l': '.-..',
-	'm': '--',
-	'n': '-.',
-	'o': '---',
-	'p': '.--.',
-	'q': '--.-',
-	'r': '.-.',
-	's': '...',
-	't': '-',
-	'u': '..-',
-	'v': '...-',
-	'w': '.--',
-	'x': '-..-',
-	'y': '-.--',
-	'z': '--..',
-	' ': '/',
-	'!': '-·-·--',
-	'.': '·-·-·-',
-	',': '--··--',
-}
 
 userEnglishInput.addEventListener('submit',  (e) => {
     e.preventDefault()
     const english = userEnglishInput.english.value.trim()
 
-    const engArray = []
-    for (let i = 0; i < english.length; i++) {
-        const eng = english[i];
-        engArray.push(eng)
-    }
+	const translation = english
+					.toLowerCase()
+					.split("")
+					.map((letter) => {
+					return alphabet[letter] ? alphabet[letter] : letter;
+					})
+					.join(" ");
+					console.log(translation);
 
-	const translation = engArray.map((letter) => {
-		return alphabet[letter]
-	})
-	if(engArray== []) {
-		returnEngTranslation.innerHTML = 'Please Type in a word'
-	}
-	console.log(returnEngTranslation.innerHTML);
 	returnEngTranslation.innerHTML = translation
 })
 
 userMorseInput.addEventListener('submit', (e) => {
 	e.preventDefault()
-	const morse = userMorseInput.morse.value.trim()
+	const morseTranslation = userMorseInput.morse.value
 	
-	const morseArray = []
-	for (let i = 0; i < morse.length; i++) {
-		const mor = morse[i];
-		morseArray.push(mor)
-	}
+	const translation = morseTranslation
+					.split(" ")
+					.map((dot) => {
+						return morse[dot] 
+						})
+					.join('')
+		
+		
 
-	const engTranslation = morseArray.map((dot) => {
-    return morse[dot]
-  })
-  console.log(alphabet['.-']);
-	returnMorseTranslation.innerHTML = engTranslation
+	returnMorseTranslation.innerHTML = translation
 })
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,129 +1,27 @@
 "use strict";
 
+var _englishData = require("./englishData.js");
+
 var userEnglishInput = document.querySelector('.userEnglish');
 var englishButton = document.querySelector('.englishButton');
 var returnEngTranslation = document.querySelector('.returnEngTranslation');
 var userMorseInput = document.querySelector('.userMorse');
 var morseButton = document.querySelector('.morseButton');
 var returnMorseTranslation = document.querySelector('.returnMorseTranslation');
-var morse = {
-  '-----': '0',
-  '.----': '1',
-  '..---': '2',
-  '...--': '3',
-  '....-': '4',
-  '.....': '5',
-  '-....': '6',
-  '--...': '7',
-  '---..': '8',
-  '----.': '9',
-  '.-': 'a',
-  '-...': 'b',
-  '-.-.': 'c',
-  '-..': 'd',
-  '.': 'e',
-  '..-.': 'f',
-  '--.': 'g',
-  '....': 'h',
-  '..': 'i',
-  '.---': 'j',
-  '-.-': 'k',
-  '.-..': 'l',
-  '--': 'm',
-  '-.': 'n',
-  '---': 'o',
-  '.--.': 'p',
-  '--.-': 'q',
-  '.-.': 'r',
-  '...': 's',
-  '-': 't',
-  '..-': 'u',
-  '...-': 'v',
-  '.--': 'w',
-  '-..-': 'x',
-  '-.--': 'y',
-  '--..': 'z',
-  '/': ' ',
-  '-·-·--': '!',
-  '·-·-·-': '.',
-  '--··--': ','
-};
-var alphabet = {
-  0: '-----',
-  1: '.----',
-  2: '..---',
-  3: '...--',
-  4: '....-',
-  5: '.....',
-  6: '-....',
-  7: '--...',
-  8: '---..',
-  9: '----.',
-  'a': '.-',
-  'b': '-...',
-  'c': '-.-.',
-  'd': '-..',
-  'e': '.',
-  'f': '..-.',
-  'g': '--.',
-  'h': '....',
-  'i': '..',
-  'j': '.---',
-  'k': '-.-',
-  'l': '.-..',
-  'm': '--',
-  'n': '-.',
-  'o': '---',
-  'p': '.--.',
-  'q': '--.-',
-  'r': '.-.',
-  's': '...',
-  't': '-',
-  'u': '..-',
-  'v': '...-',
-  'w': '.--',
-  'x': '-..-',
-  'y': '-.--',
-  'z': '--..',
-  ' ': '/',
-  '!': '-·-·--',
-  '.': '·-·-·-',
-  ',': '--··--'
-};
 userEnglishInput.addEventListener('submit', function (e) {
   e.preventDefault();
   var english = userEnglishInput.english.value.trim();
-  var engArray = [];
-
-  for (var i = 0; i < english.length; i++) {
-    var eng = english[i];
-    engArray.push(eng);
-  }
-
-  var translation = engArray.map(function (letter) {
-    return alphabet[letter];
-  });
-
-  if (engArray == []) {
-    returnEngTranslation.innerHTML = 'Please Type in a word';
-  }
-
-  console.log(returnEngTranslation.innerHTML);
+  var translation = english.toLowerCase().split("").map(function (letter) {
+    return _englishData.alphabet[letter] ? _englishData.alphabet[letter] : letter;
+  }).join(" ");
+  console.log(translation);
   returnEngTranslation.innerHTML = translation;
 });
 userMorseInput.addEventListener('submit', function (e) {
   e.preventDefault();
-  var morse = userMorseInput.morse.value.trim();
-  var morseArray = [];
-
-  for (var i = 0; i < morse.length; i++) {
-    var mor = morse[i];
-    morseArray.push(mor);
-  }
-
-  var engTranslation = morseArray.map(function (dot) {
-    return morse[dot];
-  });
-  console.log(alphabet['.-']);
-  returnMorseTranslation.innerHTML = engTranslation;
+  var morseTranslation = userMorseInput.morse.value;
+  var translation = morseTranslation.split(" ").map(function (dot) {
+    return _englishData.morse[dot];
+  }).join('');
+  returnMorseTranslation.innerHTML = translation;
 });
